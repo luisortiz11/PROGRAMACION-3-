01e1170dcf3b348d5901a7c36275054aae40000f
luisortiz11 <11ortizluis@gmail.com>
2020-01-30 21:12:44 -0500

    regex is cool?

--- tarea_2.py
+++ tarea_2.py
@@ -1,23 +1,38 @@
 #Validar una cedula
 import re
-
-#Entrada
+#Lista de provincias en orden adecuado, (indice en lista = numpro -1)
+prov = ["1-Bocas del Toro","2-Coclé", "3-Colón", "4-Chiriquí","5-Darién","6-Herrera","7-Los Santos",
+"8-Panamá", "9-Veraguas", "10-Guna Yala", "11-Emberá Wounaan","12-Ngäbe-Buglé","13-Panamá Oeste"]
+#Ciclo que se rompe cuando la entrada es una cedula valida
 while True:
-    str = input("Escribe una cedula con guiones y presiona enter: ")
-    if str.isalnum() == True:
-        print("Escribe los guiones")
-    for el in str.split("-"):
-        if el.isalnum() == False:
-            print("Solo letras y numeros")
+    str = input("Escribe una cedula con guiones y presiona enter: ") #Entrada: cualquier string
+    num = re.findall(r'\d{1,6}', str) #metodo que encuentra elementos formados por 1 a 6 digitos y crea un lista de strings con los elementos
+    letr = re.findall(r'[a-z]{1,2}', str, re.I) #metodo que encuentra elementos formados por 1 a 2 letras alfabeticas y crea un lista de strings con los elementos
+    npro = int(num[0]) #almacena el primer elemento de la lista num transformado a int. Representa la provincia de una cedula valida
+
+    if re.fullmatch(r'^\d{1,2}(pi|av|)-\d{1,4}-\d{1,5}$', str, re.I) != None: #regex que coincide con el formato de cedula tipo regular o poblacion indigena o antes de vigencia
+        if npro<=13 and npro>0: #check si el numero de provincia coincide con un elemento real
+            print("cedula valida") #mensaje de salida
+            if not letr:
+                print("tipo de cedula: Regular") #
+            elif letr[0].lower() =="pi":
+                print("tipo de cedula: Poblacion indigena") #
+            else:
+                print("tipo de cedula: Antes de la vigencia") #
+            print("provincia: " + prov[npro-1] + " tomo: " + num[1] + " asiento: " + num[2]) #
             break
+        else:
+            print("provincia invalida") #Mensaje de error
 
-
-
-        
-
-num = re.findall(r'[0-9]+', str)
-letr = re.findall(r'[a-z]+', str, re.IGNORECASE)
-
-
-
-print(num, letr)
+    elif re.fullmatch(r'^(n|pe)-\d{1,4}-\d{1,5}$|^e-\d{1,4}-\d{1,6}$', str, re.I) != None: #regex que coincide con el formato de cedula tipo naturalizado,extranjero, panameño nacido exterior
+        print("cedula valida") #mensaje de salida
+        if letr[0].lower() =="n":
+            print("tipo de cedula: Naturalizado") #
+        elif letr[0].lower() =="pe":
+            print("tipo de cedula: Panameño nacido en el extranjero") #
+        else:
+            print("tipo de cedula: Extranjero") #
+        print("tomo: " + num[0] + " asiento: " + num[1]) #
+        break
+    else:
+        print("cedula invalida") #Mensaje de error
